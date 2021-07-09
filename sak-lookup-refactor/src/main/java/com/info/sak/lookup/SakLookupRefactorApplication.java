4e0c59be49ec4212243f09bcf13ea05da14b9b15
MagdyHassan9 <Magdy.hassan.info@gmail.com>
2021-07-08 17:18:02 -0700

    init First Commit to Lookup Refactor

---
+++ sak-lookup-refactor/src/main/java/com/info/sak/lookup/SakLookupRefactorApplication.java
@@ -0,0 +1,13 @@
+package com.info.sak.lookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakLookupRefactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakLookupRefactorApplication.class, args);
+	}
+
+}
---
+++ sak-lookup-refactor/src/test/java/com/info/sak/lookup/SakLookupRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.sak.lookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakLookupRefactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
